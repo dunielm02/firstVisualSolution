811f527285b96b9466cf5d68704be57e7d7660e0
dunielm02 <dunielm02@gmail.com>
2023-01-31 00:16:44 -0500

    mobile viw finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const { urlencoded } = require('express');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontSize: {
         regular: ['12px'],
       },
+      backgroundImage:{
+        'desktop-image-hero-1': "url('../images/desktop-image-hero-1.jpg')",
+        'desktop-image-hero-2': "url('../images/desktop-image-hero-2.jpg')",
+        'desktop-image-hero-3': "url('../images/desktop-image-hero-3.jpg')",
+        'mobile-image-hero-1': "url('../images/mobile-image-hero-1.jpg')",
+        'mobile-image-hero-2': "url('../images/mobile-image-hero-2.jpg')",
+        'mobile-image-hero-3': "url('../images/mobile-image-hero-3.jpg')"
+      }
     },
     colors: {
       'Dark-Gray': "hsl(0, 0%, 63%)",
@@ -16,6 +26,10 @@ module.exports = {
     screens:{
       tablet: "375px",
       mobile: "1440px",
+    },
+    fontFamily:{
+      sans: ['spartan-500', 'sans-serif'],
+      heading: ['spartan-700', 'sans-serif']
     }
   },
   plugins: [],
